87b781c41f3848238d38af92c659a124c3f16f01
9568230 <connwalt9568@granitesd.org>
2022-09-27 12:00:52 -0600

    Update Program.cs

--- choose/Program.cs
+++ choose/Program.cs
@@ -113,7 +113,6 @@ internal class Program
         current = rand.Next(0, maxstart);
         
         Console.WriteLine("Name please!");
-        name = Console.ReadLine();
         Console.WriteLine("Pronouns?");
        
         while (true)
